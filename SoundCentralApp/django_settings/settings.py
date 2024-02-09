b3a7bf2c565a5360436516206c5f327bebb88d83
Big Schlime <h@mclane.cc>
2024-02-09 16:02:00 -0700

    enabled django-admin for updating DB also fixed some of the schema of the db

--- SoundCentralApp/django_settings/settings.py
+++ SoundCentralApp/django_settings/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'sound_central_app'
 ]
 
@@ -129,3 +130,7 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# #set the what model is used by the natural django user model
+# AUTH_USER_MODEL = 'sound_central_app.User'
+
